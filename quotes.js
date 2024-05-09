aceabfe76262bb29593ccd9cbf13973c1429192f
Stefani Nestsiarovich <stefaninesterovich@gmail.com>
2024-05-08 20:18:53 -0700

    initial commit

---
+++ quotes.js
@@ -0,0 +1,15 @@
+const localQuotes = [
+    {
+        text: 'The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart.',
+        author: 'Helen Keller',
+
+    },
+    {
+        text: 'Optimism is the faith that leads to achievement. Nothing can be done without hope and confidence.',
+        author: null,
+    },
+    {
+        text: "The trust of the innocent is the liar's most useful tool",
+        author: 'Stephen King',
+    },
+]
